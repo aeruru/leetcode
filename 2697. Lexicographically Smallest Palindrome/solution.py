4fdb3c43162cc3839d630be9926716be97749df7
Vicky Liu <41646895+aeruru@users.noreply.github.com>
2023-06-07 22:19:18 -0700

    Solution for Lexicographically Smallest Palindrome

---
+++ 2697. Lexicographically Smallest Palindrome/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    # Given a string w/ only lowercase English letters, turn the string into a 
+    # palindrome by making the smallest number of edits to the string and
+    # creating the lexographically smallest pallindrome. Lexographically smallest
+    # just means alphabetically smaller when each letter is compared from left to
+    # right.Return the pallindrome.
+    def makeSmallestPalindrome(self, s: str) -> str:
+        
+        # convert immutable string s into a list so we can modify in place
+        listS = list(s)
+        
+        # Iterate through the first half of the string, not including the middle character
+        # if the string has an odd length
+        for i in range(len(listS)//2):
+            # Use negative indices to find the corresponding character from the end of the
+            # string. So leteter at index 0 should be compared with letter at index -1,
+            # letter at index 1 compared with letter at index -2, and so on.
+            # Since we want the alphabetically smallest pallindrome string, compare the
+            # letters and use the smaller one to turn the string into a pallindrome.
+            if listS[i] < listS[-(i + 1)]:
+                listS[-(i + 1)] = listS[i]
+            else:
+                listS[i] = listS[-(i + 1)]
+        
+        # Use the join operator to create a string from the list of characters. The ''
+        # infront specfies any delimiters between characters which we don't want in this
+        # case.
+        return ''.join(listS)
