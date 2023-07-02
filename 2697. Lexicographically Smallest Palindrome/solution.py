a39314693b078e547455505364cced102bd01f91
Vicky Liu <41646895+aeruru@users.noreply.github.com>
2023-07-02 10:19:48 -0700

    Fix spelling mistakes

--- 2697. Lexicographically Smallest Palindrome/solution.py
+++ 2697. Lexicographically Smallest Palindrome/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     # Given a string w/ only lowercase English letters, turn the string into a 
     # palindrome by making the smallest number of edits to the string and
-    # creating the lexographically smallest pallindrome. Lexographically smallest
+    # creating the lexicographically smallest pallindrome. Lexicographically smallest
     # just means alphabetically smaller when each letter is compared from left to
     # right.Return the pallindrome.
     def makeSmallestPalindrome(self, s: str) -> str:
@@ -13,8 +13,8 @@ class Solution:
         # if the string has an odd length
         for i in range(len(listS)//2):
             # Use negative indices to find the corresponding character from the end of the
-            # string. So leteter at index 0 should be compared with letter at index -1,
-            # letter at index 1 compared with letter at index -2, and so on.
+            # string. The letter at index 0 should be compared with the letter at index -1,
+            # the letter at index 1 compared with the letter at index -2, and so on.
             # Since we want the alphabetically smallest pallindrome string, compare the
             # letters and use the smaller one to turn the string into a pallindrome.
             if listS[i] < listS[-(i + 1)]:
